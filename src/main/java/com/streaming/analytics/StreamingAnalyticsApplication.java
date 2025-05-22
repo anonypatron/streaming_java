99de030c638879738162cacf7b181d9eb327ef44
ShinKyu Kang <shwan1121@skuniv.ac.kr>
2025-05-22 09:48:05 +0900

    first

---
+++ src/main/java/com/streaming/analytics/StreamingAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.streaming.analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingAnalyticsApplication.class, args);
+	}
+
+}
